cd519a00ed789393ddc47da017b75dcfd946640f
Aduwo Ayooluwa <aduwoayooluwa@gmail.com>
2023-04-10 09:29:52 +0100

    working

--- src/pages/api/send_mail.ts
+++ src/pages/api/send_mail.ts
@@ -17,35 +17,40 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   }
 
 
-  
-  const { name, message, subject } = req.body as FormFields;
+  try {
+      const { name, message, subject } = req.body as FormFields;
 
-  if ( !name || !message || !subject) {
-    res.status(401).json({ error: 'Fill in the details' });
-  }
-  const courier = CourierClient(
-    { authorizationToken: process.env.COURIER_ID});
-
-  const { requestId } = await courier.send({
-    message: {
-      content: {
-        title: name,
-        body: message
-      },
-      data: {
-        message: message
-      },
-      to: {
-        email: "aduwoayooluwa2017@gmail.com"
+      if ( !name || !message || !subject) {
+        res.status(401).json({ error: 'Fill in the details' });
       }
-    }
-  });
+      const courier = CourierClient(
+        { authorizationToken: process.env.COURIER_ID});
+    
+      const { requestId } = await courier.send({
+        message: {
+          content: {
+            title: name,
+            body: message
+          },
+          data: {
+            message: message
+          },
+          to: {
+            email: "aduwoayooluwa2017@gmail.com"
+          }
+        }
+      });
+    
+      console.log(`Received form submission: ${name},  ${subject}, ${message}`);
+    
+      // Do something with the form data, such as saving it to a database
+      const messageStatus = await courier.getMessage(requestId);
+      res.status(200).json({  name, subject, message, messageStatus  });
+  }
+  catch (error) {
+    res.json({'error': error})
+  }
 
-  console.log(`Received form submission: ${name},  ${subject}, ${message}`);
-
-  // Do something with the form data, such as saving it to a database
-  const messageStatus = await courier.getMessage(requestId);
-  res.status(200).json({  name, subject, message, messageStatus  });
 };
 
 export default handler;
