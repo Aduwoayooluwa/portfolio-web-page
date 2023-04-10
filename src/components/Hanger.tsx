94add2b9adf5eaaf2a0cfaefaea57fed3a1c34c2
Aduwo Ayooluwa <aduwoayooluwa@gmail.com>
2023-04-10 10:27:32 +0100

    aninmation added

--- src/components/Hanger.tsx
+++ src/components/Hanger.tsx
@@ -1,14 +1,21 @@
 import React, { useState } from 'react'
 import { Slant as Hamburger } from 'hamburger-react'
 import { SiBlogger, SiGithub, SiHashnode, SiLinkedin, SiMicrodotblog } from 'react-icons/si'
-
+import { useSpring, animated } from 'react-spring'
 type Props = {}
 
 const Hanger = (props: Props) => {
     const [open, setOpen] = useState<boolean>(false)
+    const [showNav, setShowNav] = useState<boolean>(false)
+
+    const pageAnimation = useSpring({
+        transform: open ? 'translateX(0%)' : 'translateX(100%)',
+    });
+
+
     return (
-        <div className={`${open && 'rounded-full p-3 w-full right-0'} flex items-left flex-col  backdrop-blur-md bg-[black]/20`} >
-            <section className='w-full grid place-items-center'>
+        <animated.div className={`${open && 'rounded-full p-3 w-full right-0'} flex items-left flex-col  backdrop-blur-md bg-[black]/20`} >
+            <animated.section style={pageAnimation} className='w-full grid place-items-center'>
                 { open &&(
                     <div className='flex list-none justify-evenly items-center w-full md:w-[300px]'>
                         <li className=''><a className="text-blue-700" href='https://hashnode.com/@codingpastor'><SiHashnode fontSize={30}/></a></li>
@@ -17,13 +24,17 @@ const Hanger = (props: Props) => {
                         <li><a className='text-blue-600' href='https://linkedin.com/in/aduwo-ayooluwa'><SiLinkedin fontSize={30}/></a></li>
                     </div>)
                 }
-                <Hamburger color='white'  toggle={setOpen} toggled={open}/>
-            </section>
-            {/* {
-                !open && (<Hamburger color='white'  toggle={setOpen} toggled={open}/>)
-            } */}
+                <div className={`${!open && 'hidden'}`}>
+                    <Hamburger  color='white' easing='ease-in-out'  toggle={setOpen} toggled={open}/>
+                </div>
+                
+            </animated.section>
+            <div className={`${open && 'hidden'}`}>
+                    <Hamburger  color='white' easing='ease-in-out'  toggle={setOpen} toggled={open}/>
+            </div>
+
             
-        </div>
+        </animated.div>
     )
     }
 
