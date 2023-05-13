eb4686705a22ab902f1cbf9d7bd4e92cfbdc3925
Aduwo Ayooluwa <aduwoayooluwa@gmail.com>
2023-05-13 11:04:07 +0100

    added more projects and project page

---
+++ src/pages/projects.tsx
@@ -0,0 +1,14 @@
+import Projects from '@/components/projects/Projects'
+import React from 'react'
+
+type Props = {}
+
+const projects = (props: Props) => {
+    return (
+        <div>
+            <Projects />
+        </div>
+    )
+}
+
+export default projects
