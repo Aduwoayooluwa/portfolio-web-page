c2cd99122dea73ef641823f867f1abd635acdb24
Aduwo Ayooluwa <aduwoayooluwa@gmail.com>
2023-05-30 23:12:21 +0100

    fix/footer, width styling and added line that was ommited

---
+++ src/utils/experience.ts
@@ -0,0 +1,35 @@
+const experience = [
+    {
+        name: 'Sterling Bank PLC',
+        job_title: 'Frontend Web Developer',
+        date_joined: 'April 2022',
+        date_left: 'Present',
+        stacks: ['React', 'JavaScript', 'TypeScript', 'Next.js'],
+        website: 'https://sterling.ng'
+    },
+    {
+        name: 'Uridium Technologies',
+        job_title: 'Web Developer',
+        date_joined: 'April 2022',
+        date_left: 'Present',
+        stacks: ['React', 'JavaScript', 'TypeScript', 'Next.js', 'Node.js'],
+        website: 'https://uridumtechnologies.com'
+    },
+    {
+        name: 'The Webgate',
+        job_title: 'Digital & SEO Marketer',
+        date_joined: 'November 2021',
+        date_left: 'January 2023',
+        stacks: [''],
+        website: 'https://thewebgate.net'
+    },
+    {
+        name: 'Hashnode Blog',
+        job_title: 'Technical Writer',
+        date_joined: 'April 2022',
+        date_left: 'Present',
+        stacks: ['React', 'JavaScript', 'TypeScript', 'Next.js', 'Node.js', 'CSS', 'Tailwind', 'Astro.build'],
+        website: 'https://codingpastor.dev'
+    },
+
+]
